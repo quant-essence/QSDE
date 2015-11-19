a94cc1ea5cc9c8c2fc0821072e3226ec1b8bb4ef
OleFass <jan.ole.fass@googlemail.com>
2015-11-19 17:09:25 +0100

    initial

---
+++ OrderHistory.cpp
@@ -0,0 +1,5 @@
+/*
+ * hold & access all the orders that where triggered & closed in a linked list
+ * */
+
+#include "OrderHistory.h"
---
+++ OrderHistory.h
@@ -0,0 +1,15 @@
+//
+// Created by ole on 19.11.15.
+//
+
+#ifndef QSDE_ORDERHISTORY_H
+#define QSDE_ORDERHISTORY_H
+
+
+class OrderHistory 
+{
+
+};
+
+
+#endif //QSDE_ORDERHISTORY_H
